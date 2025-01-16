69ffc02084fa4c02db789dddc847f368707980e6
Ragavan <ragavanr738@gmail.com>
2025-01-16 12:02:06 +0530

    install stripe dependencies

--- src/bsbookings/bsbookings.module.ts
+++ src/bsbookings/bsbookings.module.ts
@@ -5,10 +5,11 @@ import { BsbookingsService } from './bsbookings.service';
 import { Booking } from './bsbookings.entity';
 import { Customers } from './../business_customers/business_customers.entity';
 import { Services } from './../bsservices/services.entity';
+import { Users } from 'src/sklyit_users/sklyit_users.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Booking, Customers, Services]), // Include related entities
+        TypeOrmModule.forFeature([Booking, Customers, Services,Users]), // Include related entities
     ],
     controllers: [BsbookingsController],
     providers: [BsbookingsService],
