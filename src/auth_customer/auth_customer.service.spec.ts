bd3d5ea5a2121f8def6fa1290c8afc9dc529cc25
chanikya nelapatla <chanikyac01@gamil.com>
2025-02-07 22:05:38 +0530

    fixed

--- src/auth_customer/auth_customer.service.spec.ts
+++ src/auth_customer/auth_customer.service.spec.ts
@@ -1,12 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthCustomerService } from './auth_customer.service';
+import { SklyitUsersService } from 'src/sklyit_users/sklyit_users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { RefreshToken } from 'src/auth/refreshtoken.entity';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthCustomerService', () => {
   let service: AuthCustomerService;
 
+  const mockSklyitUsersService = {
+    findOne: jest.fn(),
+  };
+
+  const mockRefreshTokenRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
+  const mockJwtService = {
+    sign: jest.fn(),
+    verify: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthCustomerService],
+      providers: [
+        AuthCustomerService,
+        {
+          provide: SklyitUsersService,
+          useValue: mockSklyitUsersService,
+        },
+        {
+          provide: getRepositoryToken(RefreshToken),
+          useValue: mockRefreshTokenRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthCustomerService>(AuthCustomerService);
