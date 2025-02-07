57679e1040b5fad1ec5842bae603b3ad78f6a45f
Ragavan <ragavanr738@gmail.com>
2025-02-07 22:24:03 +0530

    fixed bsservice spec

--- src/bsservices/bsservices.controller.spec.ts
+++ src/bsservices/bsservices.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BsservicesController } from './bsservices.controller';
 import { BsservicesService } from './bsservices.service';
+import { AzureBlobService } from '../imageBlob/imageBlob.service';
+
+const mockAzureBlobService = {
+  upload: jest.fn(), // Mock the upload function
+  delete: jest.fn(), // Mock the delete function
+};
 
 describe('BsservicesController', () => {
   let controller: BsservicesController;
@@ -8,7 +14,19 @@ describe('BsservicesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BsservicesController],
-      providers: [BsservicesService],
+      providers: [
+        BsservicesService,
+        {
+          provide: 'ServicesRepository', // Mock the repository as well
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([]),
+          },
+        },
+        {
+          provide: AzureBlobService, // Mocking AzureBlobService
+          useValue: mockAzureBlobService,
+        },
+      ],
     }).compile();
 
     controller = module.get<BsservicesController>(BsservicesController);
