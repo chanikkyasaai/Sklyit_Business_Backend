4101ac622e8efd0ee370a05a105964f9c18a1cea
chanikya nelapatla <chanikyac01@gamil.com>
2025-02-07 22:39:09 +0530

    auth, chat, sklyusers fixed

--- src/chat_app/chat_app.service.spec.ts
+++ src/chat_app/chat_app.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatService } from './chat_app.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Message } from './message.schema'; // Adjust the import path as necessary
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from '../sklyit_users/sklyit_users.entity'; // Adjust the import path as necessary
 
 describe('ChatService', () => {
   let service: ChatService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChatService],
+      providers: [
+        ChatService,
+        {
+          provide: getModelToken(Message.name),
+          useValue: {}, // Provide a mock implementation here
+        },
+        {
+          provide: getRepositoryToken(Users),
+          useValue: {}, // Provide a mock implementation here
+        },
+      ],
     }).compile();
 
     service = module.get<ChatService>(ChatService);
--- src/sklyit_users/sklyit_users.service.spec.ts
+++ src/sklyit_users/sklyit_users.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SklyitUsersService } from './sklyit_users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from './sklyit_users.entity'; // Adjust the import path as necessary
+import { AzureBlobService } from '../imageBlob/imageBlob.service'; // Adjust the import path as necessary
+import { ConfigService } from '@nestjs/config'; // Import ConfigService
 
 describe('SklyitUsersService', () => {
   let service: SklyitUsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SklyitUsersService],
+      providers: [
+        SklyitUsersService,
+        AzureBlobService,
+        {
+          provide: getRepositoryToken(Users),
+          useValue: {}, // Provide a mock implementation here
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockReturnValue('mocked_connection_string'), // Mock implementation for get method
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<SklyitUsersService>(SklyitUsersService);
